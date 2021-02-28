732241691e0a24d1ecf4d159c67a843c17846bfe
rocionadal <rocioonadal@gmail.com>
2021-02-28 12:04:21 +0100

    add validation component to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Validation from './Validation/Validation';
 
 class App extends Component {
   state = {
@@ -28,6 +29,7 @@ class App extends Component {
           onChange={this.inputChangeHandler} 
           value={this.state.userInput} />
         <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length} />
       </div>
     );
   }
